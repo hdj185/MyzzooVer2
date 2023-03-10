cc44b93d46131c64abe07bfa1c0ab832a3564ed4
hdj185 <hdj185583@gmail.com>
2023-03-10 18:35:07 +0900

    매도창

--- MyzzooVer2/src/View/SellView.java
+++ MyzzooVer2/src/View/SellView.java
@@ -1,23 +1,40 @@
 package View;
 
 import java.awt.BorderLayout;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
-import javax.swing.CellEditor;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingConstants;
+
+import Service.Crawler;
 
 public class SellView {
 
 	private JFrame frame;
+	private String tradingType = "매도";	//매도인지 매입인지
+	private String code = "005930";		//주식 코드
+	
 	private JPanel centerPanel;
 	private JPanel bottomPanel;
-	private JTextField quantityField;	//수량 필드
-	private JTextField priceField;		//단가 필드
+	
+	private JPanel namePanel;
+	private JPanel quantityPanel;
+	private JPanel pricePanel;
+	private JPanel calBtnPanel;
+	private JPanel calLabelPanel;
+	
+	private JSpinner quantitySpinner;	//수량 필드
+	private JSpinner priceSpinner;		//단가 필드
+	private JLabel nameLbl;	//종목 이름 라벨
+	private JLabel calLbl;	//계산 결과 라벨
 	private JButton calculBtn;
 	private JButton sellBtn;
 	private JButton cancelBtn;
@@ -28,7 +45,7 @@ public class SellView {
 
 	//창 생성
 	private void initialize() {
-		frame = new JFrame("MY ZZOO - 매도");
+		frame = new JFrame("MY ZZOO - " + tradingType);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(300, 300);
 		frame.setLocationRelativeTo(null);  
@@ -42,38 +59,96 @@ public class SellView {
 		BorderLayout layout = new BorderLayout();
 		layout.setHgap(10);
 		layout.setVgap(10);
-		frame.setLayout(layout);
+		frame.getContentPane().setLayout(layout);
 		
-		//centerPanel
+		//Panel 설정
 		centerPanel = new JPanel();
 		bottomPanel = new JPanel();
-		centerPanel.setLayout(new GridLayout(4, 2, 10, 10));
+		
+		//center, bottom 패널 레이아웃
+		centerPanel.setLayout(new GridLayout(5, 1, 10, 10));
 		bottomPanel.setLayout(new GridLayout(1, 2, 10, 10));
 		
-		quantityField = new JTextField();
-		priceField = new JTextField();
-		calculBtn = new JButton("계산");
+		//name 패널
+		namePanel = new JPanel();
+		namePanel.setLayout(new GridLayout(1, 2, 10, 10));
+		nameLbl = new JLabel("이름");
+		namePanel.add(new JLabel("종목"));
+		namePanel.add(nameLbl);
 		
-		sellBtn = new JButton("매도");
+		//quantity 패널
+		quantityPanel = new JPanel();
+		quantityPanel.setLayout(new GridLayout(1, 2, 10, 10));
+		quantitySpinner = new JSpinner(getSpinModel(1, 100000, 1));
+		quantityPanel.add(new JLabel("수량"));
+		quantityPanel.add(quantitySpinner);
+
+		//price 패널
+		pricePanel = new JPanel();
+		pricePanel.setLayout(new GridLayout(1, 2, 10, 10));
+//		priceField = new JTextField();
+		priceSpinner = new JSpinner(getSpinModel(1000, 100000, 100));
+		pricePanel.add(new JLabel("단가"));
+		pricePanel.add(priceSpinner);
+		
+		//매매 금액 패널
+		calBtnPanel = new JPanel();
+		calBtnPanel.setLayout(new GridLayout(1, 2, 10, 10));
+		calculBtn = new JButton("계산");
+		calculBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				calLbl.setText(getSellingPrice());
+			}
+		});
+		calBtnPanel.add(new JLabel("매매금액"));
+		calBtnPanel.add(calculBtn);
+		
+		//매매 금액 계산 결과 패널
+		calLabelPanel = new JPanel();
+		calLabelPanel.setLayout(new GridLayout(1, 2, 10, 10));
+		calLbl = new JLabel();
+		calLabelPanel.add(new JLabel("계산결과"));
+		calLabelPanel.add(calLbl);
+		
+		//bottom 버튼
+		sellBtn = new JButton(tradingType);
 		cancelBtn = new JButton("취소");
 		
-		centerPanel.add(new JLabel("종목"));
-		centerPanel.add(new JLabel("이름"));
-		centerPanel.add(new JLabel("수량"));
-		centerPanel.add(quantityField);
-		centerPanel.add(new JLabel("단가"));
-		centerPanel.add(priceField);
-		centerPanel.add(new JLabel("매매 금액"));
-		centerPanel.add(calculBtn);
+		//centerPanel 배치
+		centerPanel.add(namePanel);
+		centerPanel.add(quantityPanel);
+		centerPanel.add(pricePanel);
+		centerPanel.add(calBtnPanel);
+		centerPanel.add(calLabelPanel);
 		
+		//bottomPanel 배치
 		bottomPanel.add(sellBtn);
 		bottomPanel.add(cancelBtn);
 		
-		frame.add(new JLabel("매도"), BorderLayout.NORTH);
-		frame.add(centerPanel, BorderLayout.CENTER);
-		frame.add(bottomPanel, BorderLayout.SOUTH);
+		JLabel Titlelbl = new JLabel(tradingType);
+		Titlelbl.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(Titlelbl, BorderLayout.NORTH);
+		frame.getContentPane().add(centerPanel, BorderLayout.CENTER);
+		frame.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
 		
 		frame.setVisible(true);
+	}
+	
+	
+	
+	//스핀 모델 설정
+	SpinnerNumberModel getSpinModel(int init, int max, int increased) {
+		return new SpinnerNumberModel(init, 1, max, increased);	//초기값, 최소값, 최대값, 증가값 순서
+	}
+	
+	//매매단가 계산
+	String getSellingPrice() {
+		DecimalFormat df = new DecimalFormat("#,###");
+		int quantity = ((SpinnerNumberModel)quantitySpinner.getModel()).getNumber().intValue();
+		int price = ((SpinnerNumberModel) priceSpinner.getModel()).getNumber().intValue();
+		String result = df.format(quantity * price) + "원";
+		return result;
 	}
 
 }
